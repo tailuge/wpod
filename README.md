202b02f9f1c45a9e3e859aaa3007bcb931529514
tailuge <tailuge@gmail.com>
2019-06-25 18:31:02 +0000

    minimal webpack config

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # wpod
+
 minimal gitpod test
 
 [![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/tailuge/wpod)
+
+```
+yarn
+yarn deps
+yarn server
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    disableHostCheck: true,
-    host: '0.0.0.0',
-    compress: true,
-    port: 8080
-  }
+    entry: './src/main.js',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        disableHostCheck: true,
+        host: '0.0.0.0',
+        compress: true,
+        port: 8080
+    }
 };
